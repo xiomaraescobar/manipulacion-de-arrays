44ba8378ffc42af7aea4dc1280d666885f85facd
Your Name <you@example.com>
2024-07-11 21:33:00 -0500

    every

--- server/09-every.js
+++ server/09-every.js
@@ -9,10 +9,11 @@ for (let index = 0; index < numbers.length; index++) {
 }
 
 console.log('for', rta);
+
 const rta2 = numbers.every(item => item <= 40) //con el metodo every
 console.log('rta2', rta2);
 
-
+//validar si todas las edades son menores de 18 años
 const team = [
     {
         name: "Nicolas",
@@ -40,6 +41,15 @@ const team = [
     },
 ];
 
-const rta3 = team.every(member => member.age < 18) ;
+const rta3 = team.every(member => member.age < 18) ; // verificando si todos los del grupo son menores de 18
 console.log('rta3', rta3);
 
+//verificar si todos los elemntos de este array son pares
+
+function checkArray(numbers) {
+    return numbers.every(number => number % 2 === 0);
+  }
+  
+  const input1 = [2, 4, 6, 8, 10];
+  const output1 = checkArray(input1);
+  console.log(output1); 
